2a0f027ab8a381ed1a4d9465fada8c5d4ad376cd
danycondor08 <danycristophercondorsolano@gmail.com>
2025-02-27 11:04:25 -0500

    puyecto

---
+++ src/main/java/pe/edu/idat/demo_metodos_rest/Ejercicio2Controller.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_metodos_rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ejercicio2Controller {
+
+    private final Ejercicio2Service ejercicio2Service;
+
+    public Ejercicio2Controller(Ejercicio2Service ejercicio2Service) {
+        this.ejercicio2Service = ejercicio2Service;
+    }
+}
